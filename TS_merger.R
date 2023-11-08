51f742529eef78f006e9e23f4d92e0776769afe6
solar-san <solar-san@proton.me>
2023-11-09 00:11:27 +0100

    added subplots by starting year

--- TS_merger.R
+++ TS_merger.R
@@ -13,5 +13,46 @@ no_missing_values_all_ts <- all_ts_list %>%
     )
 
 no_missing_values_all_ts %>% 
-  lapply(X = ., FUN = dim) %>% 
-  unique
+  lapply(X = ., FUN = nrow ) 
+
+series_length <- c()
+for(ts in seq_along(tourism_full)){
+
+  series_length[ts] <- nrow(tourism_full) - tourism_full[, ts] %>% is.na %>% sum
+}
+
+
+
+tmelt %>% 
+  group_by(variable) %>% 
+  mutate(series_length = 43-  value %>% is.na %>% sum) %>% 
+  ungroup() %>% 
+  arrange(desc(series_length)) %>% 
+  mutate(series_length = as_factor(series_length)) %>% 
+  ggplot(
+    aes(x = Year)
+  ) +
+  facet_wrap(
+    ~series_length,
+    nrow = 6,
+    ncol = 3,
+    scales = "free"
+  ) +
+  geom_line(
+    aes(
+      y = value,
+      color = variable
+    )
+  ) +
+  scale_y_log10() +
+  theme(
+    legend.position = "none"
+  )
+  
+
+
+abc <- tmelt %>% 
+  group_by(variable) %>% 
+  mutate(series_length = 43-  value %>% is.na %>% sum) %>% 
+  arrange(desc(series_length)) %>% 
+  mutate(series_length = as_factor(series_length)) 
